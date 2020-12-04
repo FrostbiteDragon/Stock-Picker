4394771974670b795ee084633e7ed532693c6443
Cameron <cameronproulx@outlook.com>
2020-12-04 15:17:06 -0500

    spelling

--- Stock Picker/Program.cs
+++ Stock Picker/Program.cs
@@ -9,7 +9,7 @@ public static class StockPicker
     public static (int, int) PickStocks(int[] stocks)
     {
         return stocks
-        .SelectMany((_, i) => stocks.Select((_, j) => (day1: i, day2: i + j))) //tupple all possible day combonations
+        .SelectMany((_, i) => stocks.Select((_, j) => (day1: i, day2: i + j))) //tupple all possible day combination
         .Where(x => x.day2 < stocks.Length) // remove invalid days
         .OrderByDescending(x => stocks[x.Item2] - stocks[x.day1]) // order by greatest profit to lowest profit
         .First(); //take first element
