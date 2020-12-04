daf1332df5ef256b512fa77657c371d063b90553
Cameron <cameronproulx@outlook.com>
2020-12-04 15:18:31 -0500

    missed a varriable name

--- Stock Picker/Program.cs
+++ Stock Picker/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 Console.WriteLine(StockPicker.PickStocks(new int[] { 17, 3, 6, 9, 15, 8, 6, 1, 10 }));
@@ -11,7 +10,7 @@ public static class StockPicker
         return stocks
         .SelectMany((_, i) => stocks.Select((_, j) => (day1: i, day2: i + j))) //tupple all possible day combination
         .Where(x => x.day2 < stocks.Length) // remove invalid days
-        .OrderByDescending(x => stocks[x.Item2] - stocks[x.day1]) // order by greatest profit to lowest profit
+        .OrderByDescending(x => stocks[x.day2] - stocks[x.day1]) // order by greatest profit to lowest profit
         .First(); //take first element
     }
 }
